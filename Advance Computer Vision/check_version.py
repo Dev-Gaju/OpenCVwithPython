3755ca84ac78f56fe96b48943a94321bec9b5575
Dev-Gaju <gajuahmd@gmail.com>
2021-11-09 21:15:05 +0600

    update

--- Advance Computer Vision/check_version.py
+++ Advance Computer Vision/check_version.py
@@ -1,2 +1,23 @@
 import cv2
 print(cv2.__version__)
+
+
+
+#
+# x=2
+# y=3
+# w=2
+# h=5
+#
+# # a=(x+w)
+# # b=(y + h)
+# a,b=(x + w, y + h)
+#
+# print(a, type(a))
+a=0
+x, y=50, 50
+
+print(type(x))
+
+x,y= a.pos
+# x = [i[0] for i in button.pos]
